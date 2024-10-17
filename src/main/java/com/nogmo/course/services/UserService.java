08e03be05216220d311ce0626340a3f55d3300b7
Henrique Santos <henriquersantos99@outlook.com>
2024-10-16 21:51:49 -0300

    Exception handling - findById

--- src/main/java/com/nogmo/course/services/UserService.java
+++ src/main/java/com/nogmo/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.nogmo.course.entities.User;
 import com.nogmo.course.repositories.UserRepository;
+import com.nogmo.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long Id) {
 		Optional<User> obj = repository.findById(Id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(Id));
 	}
 	
 	public User insert(User obj) {
